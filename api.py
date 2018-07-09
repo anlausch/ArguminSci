9cf5d7d01780473fc4eb38d314bc5506a734f93f
anlausch <anne-lauscher@web.de>
2018-07-09 11:23:27 +0200

    First version with arg and discourse

--- api.py
+++ api.py
@@ -5,8 +5,11 @@ import traceback
 import logging
 from flask_bootstrap import Bootstrap
 import serve
-
+from werkzeug.utils import secure_filename
 import json
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['txt'])
 
 class Server:
     def __init__(self, **kwds):
@@ -14,13 +17,37 @@ class Server:
 
 server = Server()
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 Bootstrap(app)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def connect_spans(result):
+    new_result = []
+    for sentence in result:
+        new_sentence = []
+        for i,word_label in enumerate(sentence):
+            word_label[1] = assign_style_classes(word_label[1])
+            new_sentence.append(word_label)
+            if i != len(sentence)-1:
+                if word_label[1] == assign_style_classes(sentence[i+1][1]):
+                    new_sentence.append([" ", word_label[1]])
+                else:
+                    new_sentence.append([" ", ""])
+        new_result.append(new_sentence)
+    return new_result
+
+
+
 
 def assign_style_classes(label):
     # map the inputs to the function blocks
     options = {
-        "Token_Label.BEGIN_BACKGROUND_CLAIM" : "background_claim",
-        "Token_Label.INSIDE_BACKGROUND_CLAIM" : "background_claim",
+        "Token_Label.BEGIN_BACKGROUND_CLAIM": "background_claim",
+        "Token_Label.INSIDE_BACKGROUND_CLAIM": "background_claim",
         "Token_Label.BEGIN_OWN_CLAIM": "own_claim",
         "Token_Label.INSIDE_OWN_CLAIM": "own_claim",
         "Token_Label.BEGIN_DATA": "data",
@@ -31,7 +58,8 @@ def assign_style_classes(label):
         "DRI_Challenge": "challenge",
         "DRI_Background": "background",
         "DRI_FutureWork": "future_work",
-        "DRI_Unspecified": ""
+        "DRI_Unspecified": "",
+        "": ""
     }
     return options[label]
 
@@ -44,14 +72,26 @@ def predict():
                 logger.error("No data provided")
                 return render_template("index.html", error="Please insert a text before submitting.")
             logger.info("Data: " + json.dumps(text))
-            result = serve.predict(text=text, embd_vocab=server.embd_vocab, model=server.model)
-            logger.info("Result: " + json.dumps(result))
 
-            result = [['<span class="'+ assign_style_classes(word_label[1]) + '">' + str(word_label[0]) + '</span>' for j, word_label in enumerate(sentence)] for i, sentence in enumerate(result)]
+            argumentation = server.argumentation.predict(text=text)
+            discourse = server.discourse.predict(text=text)
 
-            result = [' '.join(sentence) for sentence in result]
-            result = '. '.join(result)
-            return render_template("index.html", result=result)
+            logger.info("Argumentation: " + json.dumps(argumentation))
+            logger.info("Discourse: " + json.dumps(discourse))
+
+            argumentation = connect_spans(argumentation)
+            discourse = connect_spans(discourse)
+
+            argumentation = [['<span class="' + word_label[1] + '">' + str(word_label[0]) + '</span>' for j, word_label in enumerate(sentence)] for i, sentence in enumerate(argumentation)]
+            discourse = [['<span class="' + word_label[1] + '">' + str(word_label[0]) + '</span>' for j, word_label in
+                       enumerate(sentence)] for i, sentence in enumerate(discourse)]
+
+            discourse = [''.join(sentence) for sentence in discourse]
+            argumentation = [''.join(sentence) for sentence in argumentation]
+
+            argumentation = ' '.join(argumentation)
+            discourse = ' '.join(discourse)
+            return render_template("index.html", argumentation=argumentation, discourse=discourse)
 
         else:
             text = request.form["text"]
@@ -108,12 +148,9 @@ def exceptions(e):
                   tb)
     return "Internal Server Error", 500
 
+
 if __name__ == '__main__':
-    model = serve.load_model()
-    embd_dict, embedding_vocab = serve.load_embeddings()
-    server = Server(model=model,
-                 embd_dict=embd_dict,
-                 embd_vocab=embedding_vocab)
+    server = Server(discourse=serve.Model("discourse"), argumentation=serve.Model("argumentation"))
 
     handler = RotatingFileHandler('./log/app.log', maxBytes=10000, backupCount=3)
     logger = logging.getLogger(__name__)
--- serve.py
+++ serve.py
@@ -1,183 +1,177 @@
 import tensorflow as tf
-import load_conll
-import utils
 import numpy as np
 import os
 import re
 import codecs
 from nltk import word_tokenize
 from nltk import sent_tokenize
+import pickle
 
 class Model:
-    def __init__(self, **kwds):
-        self.__dict__.update(kwds)
+    def __init__(self, type):
+        self.type = type
+        self.path = os.path.join("./model", type)
+        tf.reset_default_graph()
+        self.graph = tf.Graph()
+        self.sess = tf.Session(graph=self.graph)
+        with self.graph.as_default():
+            self.saver = tf.train.import_meta_graph(self.path + '/best-model.meta')
+            self.saver.restore(self.sess, tf.train.latest_checkpoint(self.path))
+            self.input_tensor = self.graph.get_tensor_by_name('input_x:0')
+            self.dropout_keep_prob_tensor = self.graph.get_tensor_by_name('dropout_keep_prob:0')
+            self.sequence_lengths_tensor = self.graph.get_tensor_by_name('Placeholder:0')
+            if type == "argumentation":
+                self.arg_max = self.graph.get_tensor_by_name("arg_prediction/ArgMax:0")
+            elif type == "discourse":
+                self.arg_max = self.graph.get_tensor_by_name("auxiliary_prediction/ArgMax:0")
+
+            self.embedding_dict, self.embedding_vocab = self.load_embeddings(self.path)
 
 
-def is_number(token):
-	return re.match('^[\d]+[,]*.?\d*$', token) is not None
+    def is_number(self, token):
+        return re.match('^[\d]+[,]*.?\d*$', token) is not None
 
 
-def punctuation():
-	return ['—', '-', '.', ',', ';', ':', '\'', '"', '{', '}', '(', ')', '[', ']']
+    def punctuation(self):
+        return ['—', '-', '.', ',', ';', ':', '\'', '"', '{', '}', '(', ')', '[', ']']
 
 
-def assign_labels(predictions):
-    string_predictions=[]
-    with codecs.open("./model/config.txt", 'rb', "utf8") as file:
-        for line in file.readlines():
-            parts = str(line).split(": ")
-            if parts[0] == "main labels":
-                labels = eval(parts[1])
-                break
-        for i,sentence in enumerate(predictions):
-            string_predictions.append([])
-            for j,token in enumerate(sentence):
-                for k,label in enumerate(labels):
-                    if token == k:
-                        string_predictions[i].append(label)
-                        break
-        return string_predictions
+    def begin_of_span(self):
+        return ["Token_Label.BEGIN_BACKGROUND_CLAIM", "Token_Label.BEGIN_OWN_CLAIM", "Token_Label.BEGIN_DATA"]
 
 
-def prepare_sequence_labelling_text(texts, embeddings_vocab, pad=True, pad_token='<PAD/>', numbers_token='<NUM/>', punct_token="<PUNC/>"):
-    x = []
-    for i in range(len(texts)):
-        if i % 2 == 0:
-            print("Line: " + str(i) + " of " + str(len(texts)))
-        tok_list = []
-        for j in range(len(texts[i])):
-            token_clean = texts[i][j]
-            token = token_clean
-            if token_clean.strip() in punctuation() and punct_token is not None:
-                token = punct_token
-            if is_number(token_clean) and numbers_token is not None:
-                token = numbers_token
-            if token not in embeddings_vocab and token.lower() not in embeddings_vocab:
-                continue
-            tok_list.append(embeddings_vocab[token] if token in embeddings_vocab else embeddings_vocab[token.lower()])
-        x.append(tok_list)
-    print("Line: " + str(len(texts)) + " of " + str(len(texts)))
-    sequence_lengths = [len(sentence) for sentence in x]
-    if pad:
-        ind_pad = embeddings_vocab[pad_token]
-        max_len = 1343 #max([len(t) for t in x])
-        x = [t + [ind_pad] * (max_len - len(t)) for t in x]
-    return np.array(x, dtype=np.int32), np.array(sequence_lengths, dtype=np.int32)
-
-
-
-def prepare_embeddings(embd_dict, pad_token, num_token, punct_token, word2vec=False):
-    embeddings_words2index = dict()
-    if word2vec == True:
-        embedding_matrix = np.zeros((len(embd_dict.vocab) + 3, len(embd_dict["word"])))
-        for i in range(len(embd_dict.vocab)):
-            embedding_vector = embd_dict[embd_dict.index2word[i]]
-            embeddings_words2index[embd_dict.index2word[i]] = i
-            if embedding_vector is not None:
-                embedding_matrix[i] = embedding_vector
+    def assign_labels(self, predictions):
+        string_predictions=[]
+        with codecs.open(os.path.join(self.path, "config.txt"), 'rb', "utf8") as file:
+            for line in file.readlines():
+                parts = str(line).split(": ")
+                if parts[0] == "main labels":
+                    labels = eval(parts[1])
+                    break
+            if self.type == "argumentation":
+                for i,sentence in enumerate(predictions):
+                    string_predictions.append([])
+                    for j,token in enumerate(sentence):
+                        for k,label in enumerate(labels):
+                            if token == k:
+                                string_predictions[i].append(label)
+                                break
             else:
-                print("Problem with word vector")
-        embedding_matrix[len(embd_dict.vocab)] = np.random.uniform(-1.0, 1.0, len(embd_dict["word"]))
-        embeddings_words2index[pad_token] = len(embd_dict.vocab)
+                for i,sentence in enumerate(predictions):
+                    string_predictions.append([])
+                    for k, label in enumerate(labels):
+                        if sentence == k:
+                            string_predictions[i].append(label)
+                            break
+            return string_predictions
 
-        embedding_matrix[len(embd_dict.vocab)+1] = np.random.uniform(-1.0, 1.0, len(embd_dict["word"]))
-        embeddings_words2index[num_token] = len(embd_dict.vocab)+ 1
 
-        embedding_matrix[len(embd_dict.vocab)+2] = np.random.uniform(-1.0, 1.0, len(embd_dict["word"]))
-        embeddings_words2index[punct_token] = len(embd_dict.vocab) +2
-
-        print("Embeddings are loaded and aligned")
-    else:
-        embedding_matrix = np.zeros((len(embd_dict.keys()) + 3, len(embd_dict["word"])))
-        for i, (word, vector) in enumerate(embd_dict.items()):
-            embeddings_words2index[word] = i
-            if vector is not None:
-                embedding_matrix[i] = vector
+    def prepare_sequence_labelling_text(self, texts, pad=True, pad_token='<PAD/>', numbers_token='<NUM/>', punct_token="<PUNC/>"):
+        x = []
+        punctuation_list = []
+        numbers_list = []
+        removed_tokens = {}
+        for i in range(len(texts)):
+            if i % 2 == 0:
+                print("Line: " + str(i) + " of " + str(len(texts)))
+            tok_list = []
+            for j in range(len(texts[i])):
+                token_clean = texts[i][j]
+                token = token_clean
+                if token_clean.strip() in self.punctuation() and punct_token is not None:
+                    punctuation_list.append(token)
+                    token = punct_token
+                if self.is_number(token_clean) and numbers_token is not None:
+                    numbers_list.append(token)
+                    token = numbers_token
+                if token not in self.embedding_vocab and token.lower() not in self.embedding_vocab:
+                    removed_tokens[str(i) + "_" + str(j)] = token
+                    continue
+                tok_list.append(self.embedding_vocab[token] if token in self.embedding_vocab else self.embedding_vocab[token.lower()])
+            x.append(tok_list)
+        print("Line: " + str(len(texts)) + " of " + str(len(texts)))
+        sequence_lengths = [len(sentence) for sentence in x]
+        if pad:
+            ind_pad = self.embedding_vocab[pad_token]
+            if self.type == "argumentation":
+                max_len = 167 #max([len(t) for t in x])
             else:
-                print("Problem with word vector")
-        embedding_matrix[len(embd_dict.keys())] = np.random.uniform(-1.0, 1.0, len(embd_dict["word"]))
-        embeddings_words2index[pad_token] = len(embd_dict.keys())
+                max_len = 1343
+            x = [t + [ind_pad] * (max_len - len(t)) for t in x]
+            real_length = len(x)
+            while len(x) % 16 != 0:
+                x.append([ind_pad for i in range(max_len)])
+                sequence_lengths.append(0)
 
-        embedding_matrix[len(embd_dict.keys())+1] = np.random.uniform(-1.0, 1.0, len(embd_dict["word"]))
-        embeddings_words2index[num_token] = len(embd_dict.keys()) + 1
-
-        embedding_matrix[len(embd_dict.keys())+2] = np.random.uniform(-1.0, 1.0, len(embd_dict["word"]))
-        embeddings_words2index[punct_token] = len(embd_dict.keys()) + 2
-
-    return embedding_matrix, embeddings_words2index
+        return np.array(x, dtype=np.int32), np.array(sequence_lengths, dtype=np.int32), np.array(punctuation_list), \
+               np.array(numbers_list), removed_tokens, real_length
 
 
-def load_model():
-    sess = tf.Session()
-    saver = tf.train.import_meta_graph('./model/best-model.meta')
-    saver.restore(sess, tf.train.latest_checkpoint('./model/'))
-    graph = tf.get_default_graph()
-    input_x_tensor = graph.get_tensor_by_name("input_x:0")
-    dropout_keep_prob_tensor = graph.get_tensor_by_name("dropout_keep_prob:0")
-    sequence_lengths_tensor = graph.get_tensor_by_name("Placeholder:0")
-    arg_max = graph.get_tensor_by_name("arg_prediction/ArgMax:0")
-    model = Model(input_x_tensor=input_x_tensor,
-                  dropout_keep_prob_tensor=dropout_keep_prob_tensor,
-                  sequence_lengths_tensor=sequence_lengths_tensor,
-                  arg_max = arg_max,
-                  graph = graph,
-                  sess = sess)
-    return model
+    def load_embeddings(self, path):
+        embedding_vocab = pickle.load(open(path + "/embedding_vocab", "rb"))
+        embedding_dict = pickle.load(open(path + "/embeddings", "rb"))
+        return embedding_dict, embedding_vocab
 
 
-def load_embeddings():
-    if os.name == "nt":
-        # embd_dict = utils.load_embeddings(
-        #    "C:/Users/anlausch/workspace/cnn-text-classification/data/GoogleNews-vectors-negative300.bin", word2vec=True)
-        embd_dict = utils.load_embeddings("C:/Users/anlausch/workspace/embedding_files/glove.6B/glove.6B.50d.txt",
-                                          word2vec=False)
-    else:
-        # embd_dict = utils.load_embeddings("~/GoogleNews-vectors-negative300.bin", word2vec=True)
-        embd_dict = utils.load_embeddings("./glove.6B.300d.txt", word2vec=False)
-    embeddings, embedding_vocab = prepare_embeddings(embd_dict, pad_token="<PAD/>", num_token="<NUM/>",
-                                                     punct_token="<PUNC/>", word2vec=False)
-    return embd_dict, embedding_vocab
+    def rereplace_puncuation_and_numbers(self, result, punctuation_list, numbers_list, removed_tokens):
+        count_puncts = 0
+        count_nums = 0
+        for sentence in result:
+            for word_label in sentence:
+                if word_label[0] == "<PUNC/>":
+                    word_label[0] = punctuation_list[count_puncts]
+                    count_puncts = count_puncts + 1
+                elif word_label[0] == "<NUM/>":
+                    word_label[0] = numbers_list[count_nums]
+                    count_nums = count_nums + 1
+        for key, value in removed_tokens.items():
+            i, j = key.split("_")
+            result[int(i)].insert(int(j), [value, "REPLACED"])
+        for i, sentence in enumerate(result):
+            for j, word_label in enumerate(sentence):
+                if word_label[1] == "REPLACED":
+                    if j == 0 and j != len(sentence)-1 and sentence[j+1][1] not in self.begin_of_span():
+                        word_label[1] = sentence[j+1][1]
+                    elif j != len(sentence)-1 and sentence[j-1][1] == sentence[j+1][1] and sentence[j+1][1] not in self.begin_of_span():
+                        word_label[1] = sentence[j+1][1]
+                    elif j == len(sentence)-1 and sentence[j-1][1]:
+                        word_label[1] = sentence[j-1][1]
+                    else:
+                        word_label[1] = "Token_Label.OUTSIDE"
+        return result
 
 
-def predict(text, embd_vocab, model):
-    text = [word_tokenize(sentence) for sentence in sent_tokenize(text)]
-    input, sequence_lengths = prepare_sequence_labelling_text(texts=text, embeddings_vocab=embd_vocab)
+    def predict(self, text):
+        text = [word_tokenize(sentence) for sentence in sent_tokenize(text)]
+        input, sequence_lengths, punctuation_list, numbers_list, removed_tokens, real_length = self.prepare_sequence_labelling_text(texts=text)
 
-    feed_dict = {
-        model.input_x_tensor: input, model.dropout_keep_prob_tensor: 1.0,
-        model.sequence_lengths_tensor: sequence_lengths
-    }
+        grouped_input = [input[n:n + 16] for n in range(0, len(input), 16)]
+        grouped_sequence_lengths = [sequence_lengths[n:n + 16] for n in range(0, len(sequence_lengths), 16)]
 
-    labels = model.sess.run(model.arg_max, feed_dict)
-    index_to_word = {v: k for k, v in embd_vocab.items()}
-    labels = assign_labels(labels)
-    result = [[[index_to_word[word], labels[i][j]] for j, word in enumerate(sentence) if j < sequence_lengths[i]] for i, sentence in
-              enumerate(input)]
-    return result
+        labels = []
+        for i, group in enumerate(grouped_input):
+            feed_dict = {
+                self.input_tensor: group,
+                self.dropout_keep_prob_tensor: 1.0,
+                self.sequence_lengths_tensor: grouped_sequence_lengths[i]
+            }
 
+            labels.append(self.sess.run(self.arg_max, feed_dict))
+
+        labels = [item for sublist in labels for item in sublist]
+        index_to_word = {v: k for k, v in self.embedding_vocab.items()}
+        labels = self.assign_labels(labels)
+        if type == "argumentation":
+            result = [[[index_to_word[word], labels[i][j]] for j, word in enumerate(sentence) if j < sequence_lengths[i]] for i, sentence in
+                      enumerate(input) if i < real_length]
+        else:
+            result = [[[index_to_word[word], labels[i][0]] for j, word in enumerate(sentence) if j < sequence_lengths[i]] for i, sentence in
+                      enumerate(input) if i < real_length]
+        result = self.rereplace_puncuation_and_numbers(result, punctuation_list, numbers_list, removed_tokens)
+        return result
 
 def main():
-    model = load_model()
-    embd_dict, embedding_vocab = load_embeddings()
-
-
-    x, y_arg, y_rhet, y_aspect, y_summary, y_citation = load_conll.load_data_multiple("./annotations_conll_final_splitted/test")
-
-    x, y_arg, classes_arg, y_rhet, classes_rhet, sequence_lengths, y_aspect, classes_aspect, y_summary, classes_summary, y_citation, classes_citation = prep_sequence_labelling(
-        texts=x, labels_main=y_arg, labels_aux=y_rhet, labels_aux2=y_aspect, labels_aux3=y_summary,
-        labels_aux4=y_citation, embeddings_vocab=embedding_vocab, pad=True, pad_token="<PAD/>")
-
-
-
-    x = x[:16]
-    y_arg = y_arg[:16]
-    sequence_lengths = sequence_lengths[:16]
-
-    feed_dict ={model.input_x_tensor: x, model.dropout_keep_prob_tensor: 1.0, model.sequence_lengths_tensor: sequence_lengths}
-
-    result = model.sess.run(model.arg_max,feed_dict)
-    result = assign_labels(result)
-    print(result)
+    print("Started")
 
 
 if __name__=='__main__':
